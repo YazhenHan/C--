49c38833ea69189bbf680bff91445c50bdc74b67
HanYazhen <yazhenhan@foxmail.com>
2020-11-14 23:59:31 +0800

    Leetcode 599 maxDepth

---
+++ Leetcode/MaxDepth/MaxDepth.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution
+{
+public:
+    int maxDepth(Node *root)
+    {
+        if (root == nullptr)
+        {
+            return 0;
+        }
+        int res = 0;
+        for(auto &node : root->children) {
+            int temp = maxDepth(node);
+            if (temp > res)
+            {
+                res = temp;
+            }
+        }
+        return res + 1;
+    }
+};
