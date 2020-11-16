089f1a5e67ebaa9de80f491eb9ca1b61ddabdd60
HanYazhen <yazhenhan@foxmail.com>
2020-11-16 21:02:06 +0800

    Unfinished Leetcode 406

---
+++ Leetcode/ReconstructQueue/ReconstructQueue.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        
+    }
+};
+
+int main() {
+
+}
