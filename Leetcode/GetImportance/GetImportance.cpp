3f74498acc40a22d285cd9268b027612086324e8
HanYazhen <yazhenhan@foxmail.com>
2020-12-03 16:58:28 +0800

    Leetcode 690

---
+++ Leetcode/GetImportance/GetImportance.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+using namespace std;
+
+class Employee
+{
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+
+class Solution
+{
+public:
+    int getImportance(vector<Employee *> employees, int id)
+    {
+        int res = 0;
+        for (int i = 0; i < employees.size(); i++)
+        {
+            if (employees[i]->id == id)
+            {
+                res = employees[i]->importance;
+                for (int j = 0; j < employees[i]->subordinates.size(); j++)
+                {
+                    res += getImportance(employees, employees[i]->subordinates[j]);
+                }
+                break;
+            }
+        }
+        return res;
+    }
+};
