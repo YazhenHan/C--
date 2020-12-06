181e6d80df213fd8a90616a8b8c56de4a8c357fc
HanYazhen <33389819+HanYazhen@users.noreply.github.com>
2020-12-06 09:12:11 +0800

    Leetcode 118

---
+++ Leetcode/Generate/Generate.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        for (int i = 1; i <= numRows; i++)
+        {
+            vector<int> temp;
+            temp.push_back(1);
+            for (int j = 2; j < i; j++)
+            {
+                temp.push_back(res[i-1][j-1] + res[i-1][j]);
+            }
+            temp.push_back(1);
+            res.push_back(temp);
+        }
+        return res;
+    }
+};
