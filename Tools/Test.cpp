35f78ad882c81fabe8c8290b15933184187ca82d
HanYazhen <yazhenhan@foxmail.com>
2020-12-07 20:49:51 +0800

    Matrix and Vector Tools

--- Tools/Test.cpp
+++ Tools/Test.cpp
@@ -3,26 +3,12 @@
 
 int main()
 {
-    std::vector<std::vector<int>> a = {{1, 2, 3}, {4, 5, 6}};
-    std::vector<std::vector<int>> b = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    std::vector<std::vector<double>> a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    std::vector<std::vector<int>> b = {{1, 2, 3}, {4, 5, 6}};
+
     std::vector<int> x = {1, 2, 3};
-    std::vector<int> y = {4, 5, 6};
-
-    std::cout << Tools::dot(x, y) << std::endl << std::endl;
-
-    std::vector<std::vector<int>> res;
-    res = Tools::mult(a, b);
-    Tools::outTVector(res);
-
-    res = Tools::transpose(a);
-    Tools::outTVector(res);
-
-    std::vector<int> resres;
-    resres = Tools::mult(a, x);
-    Tools::outVector(resres);
-
-    resres = Tools::mult(x, b);
-    Tools::outVector(resres);
+    std::vector<double> y = {4.5, 5.0};
+    std::cout << Tools::mult(a, a) << std::endl;
 
     return 0;
 }
