ac822397bcf12b03bdbf3220e2cf75eeb98b9883
HanYazhen <yazhenhan@foxmail.com>
2020-12-12 22:18:59 +0800

    Sort

--- Algorithms/Sort/Sort.cpp
+++ Algorithms/Sort/Sort.cpp
@@ -1,29 +1,61 @@
-#include <cstdlib>
 #include <iostream>
+#include <vector>
+#include "../../Tools/Tools.hpp"
 
-template<class T>
-class Sort
-{
-public:
-    T* a;
-public:
-    Sort(int size) {
-        a = (T*)malloc(size * sizeof(T));
+void exch(std::vector<int>& nums, int i, int j) {
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+
+void selectSort(std::vector<int>& nums) {
+    int N = nums.size();
+    for (int i = 0; i < N; i++)
+    {
+        int min = i;
+        for (int j = i; j < N; j++)
+            if (nums[j] < nums[min])
+                min = j;
+        exch(nums, i, min);
     }
-    ~Sort() {
-        free(a);
+}
+
+void insertSort(std::vector<int>& nums) {
+    int N = nums.size();
+    for (int i = 0; i < N; i++)
+    {
+        int j = i - 1, temp = nums[i];
+        for (; j >= 0 && nums[j] > temp; j--)
+            nums[j+1] = nums[j];
+        nums[j+1] = temp;
     }
-};
+}
+
+void shellSort(std::vector<int>& nums) {
+    int N = nums.size();
+    int h = 1;
+    while (h < N / 3)
+    {
+        h = 3 * h + 1;
+    }
+    while (h >= 1)
+    {
+        for (int i = h; i < N; i++)
+            for (int j = i; j >= h && nums[j] < nums[j-h]; j -= h)
+                exch(nums, j, j-h);
+        h = h / 3;
+    }
+}
 
 int main() {
-    Sort<int> a = Sort<int>(5);
-    for (int i = 0; i < 5; i++)
-    {
-        a.a[i] = i;
-    }
-    for (int i = 0; i < 5; i++)
-    {
-        std::cout << a.a[i] << " ";
-    }
+    std::vector<int> nums = {1,4,4,-2,3};
+    insertSort(nums);
+    std::cout << nums << std::endl;
+    nums = {1,4,4,-2,3};
+    selectSort(nums);
+    std::cout << nums << std::endl;
+    nums = {1,4,4,-2,3};
+    shellSort(nums);
+    std::cout << nums << std::endl;
     return 0;
 }
