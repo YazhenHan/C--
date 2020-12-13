bb7eb0cec6f91a5b13d70b201a9750fea02d6a2e
HanYazhen <yazhenhan@foxmail.com>
2020-12-13 10:36:04 +0800

    Leetcode 217

---
+++ Leetcode/ContainsDuplicate/ContainsDuplicate.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> us;
+        for (auto num : nums)
+            if (us.find(num) == us.end())
+                us.insert(num);
+            else
+                return true;
+        return false;
+    }
+};
