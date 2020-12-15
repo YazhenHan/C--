a9e663b9f61b6ce1d3e3337328d84603310587c6
HanYazhen <yazhenhan@foxmail.com>
2020-12-15 14:31:04 +0800

    List.cpp

--- Algorithms/List/List.cpp
+++ Algorithms/List/List.cpp
@@ -1,32 +1,47 @@
 #include <iostream>
 
 template <typename T>
-struct Node {
-    T val;
-    Node* next;
-    // Node(T x) : val(x), next(nullptr) {}
+class List
+{
+private:
+    struct Node {
+        T val;
+        Node* next = nullptr;
+        Node(T val, Node* &tail, Node* &head) : val(val) {
+            if (tail == nullptr)
+                tail = head = this;
+            tail = tail->next = this;
+        }
+    };
+    Node* head = nullptr;
+    Node* tail = nullptr;
+public:
+    void put(T val) {
+        new Node(val, tail, head);
+        return;
+        // if (tail == nullptr)
+        //     tail = head = new Node(val);
+        // else
+        //     tail = tail->next = new Node(val);
+    }
+    T get() {
+        if (head == nullptr)
+            return 0;
+        Node* tempPtr = head;
+        head = head->next;
+        int temp = tempPtr->val;
+        free(tempPtr);
+        return temp;
+    }
 };
 
 int main() {
-    int temp;
-    std::cin >> temp;
-    Node<int>* head = new Node<int>();
-    head->val = temp;
-    head->next = nullptr;
-    Node<int>* head2 = head;
-    while (std::cin >> temp)
-    {
-        Node<int>* tempNode = new Node<int>();
-        tempNode->val = temp;
-        tempNode->next = nullptr;
-        head2->next = tempNode;
-        head2 = head2->next;
-    }
-    while (head != nullptr) 
-    {
-        std::cout << head->val << " ";
-        head = head->next;
-    }
-    std::cout << std::endl;
+    List<int> list;
+    list.put(1);
+    list.put(2);
+    list.put(3);
+    std::cout << list.get() << std::endl;
+    std::cout << list.get() << std::endl;
+    std::cout << list.get() << std::endl;
     return 0;
 }
