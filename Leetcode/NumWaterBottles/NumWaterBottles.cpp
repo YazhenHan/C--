dd4ab20eb0680040662ae04e82a57a0e42811f4e
HanYazhen <yazhenhan@foxmail.com>
2020-12-17 12:08:57 +0800

    Leetcode 1518

--- Leetcode/NumWaterBottles/NumWaterBottles.cpp
+++ Leetcode/NumWaterBottles/NumWaterBottles.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
         int res = numBottles;
-        numBottles = numBottles / numExchange + numBottles % numExchange;
         while (numBottles >= numExchange)
         {
             res += numBottles / numExchange;
