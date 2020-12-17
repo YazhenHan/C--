46d7b87019a65fe5ac6736e6cdd8495c1b582028
HanYazhen <yazhenhan@foxmail.com>
2020-12-17 12:40:32 +0800

    Leetcode 976

--- Leetcode/LargestPerimeter/LargestPerimeter.cpp
+++ Leetcode/LargestPerimeter/LargestPerimeter.cpp
@@ -8,9 +8,12 @@ public:
     int largestPerimeter(vector<int>& A) {
         sort(A.begin(), A.end());
         int res = 0;
-        for (int i = A.size() - 1; i >= 0; i--)
+        for (int i = A.size() - 1; i - 2 >= 0; i--)
             if (A[i] - A[i - 1] < A[i - 2])
+            {
                 res = A[i] + A[i - 1] + A[i-2];
+                break;
+            }
         return res;
     }
 };
