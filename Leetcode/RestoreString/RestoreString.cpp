a424db02618e3b8112dc9d2265fd26b8b60cee62
HanYazhen <33389819+HanYazhen@users.noreply.github.com>
2020-12-18 10:55:16 +0800

    Leetcode 1528

--- Leetcode/RestoreString/RestoreString.cpp
+++ Leetcode/RestoreString/RestoreString.cpp
@@ -12,4 +12,22 @@ public:
             res[indices[i]] = s[i];
         return res;
     }
+
+    string restoreString(string s, vector<int>& indices) {
+        for (int i = 0; i < s.size(); i++)
+        {
+            int j = i;
+            char temp = s[j];
+            char temp2 = s[indices[j]];
+            while (i != indices[j])
+            {
+                s[indices[j]] = temp;
+                j = indices[j];
+                temp = temp2;
+                temp2 = s[indices[j]];
+            }
+            s[indices[j]] = temp;
+        }
+        return s;
+    }
 };
