2dd2512fb47443f97dbb3da5fb4dbf0fd1d937f7
HanYazhen <yazhenhan@foxmail.com>
2020-12-18 09:46:39 +0800

    Leetcode 1528

---
+++ Leetcode/RestoreString/RestoreString.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string res;
+        res.resize(s.size());
+        for (int i = 0; i < s.size(); i++)
+            res[indices[i]] = s[i];
+        return res;
+    }
+};
