6ccf72d4bb3e93d5ec16995cb618d2bcac5ab388
HanYazhen <yazhenhan@foxmail.com>
2020-12-18 09:36:19 +0800

    Leetcode 1502

--- Leetcode/CanMakeArithmeticProgression/CanMakeArithmeticProgression.cpp
+++ Leetcode/CanMakeArithmeticProgression/CanMakeArithmeticProgression.cpp
@@ -8,13 +8,8 @@ public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
         sort(arr.begin(), arr.end());
         for (int i = 1; i + 1 < arr.size(); i++)
-        {
             if (arr[i] - arr[i-1] != arr[i+1] - arr[i])
-            {
                 return false;
-            }
-            
-        }
         return true;
     }
 };
