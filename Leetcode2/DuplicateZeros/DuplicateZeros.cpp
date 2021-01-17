f4cb4e93ffba98e1e8797eb222d7ada81dc89e93
YazhenHan <yazhenhan@foxmail.com>
2021-01-17 22:46:28 +0800

    Leetcode 1089

---
+++ Leetcode2/DuplicateZeros/DuplicateZeros.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int count = 0;
+        for (auto a : arr)
+            a == 0 ? count++ : count;
+        for (int i = arr.size() - 1; i >= 0; i--)
+        {
+            if (i + count < arr.size())
+            {
+                arr[i + count] = arr[i];
+            }
+            if (arr[i] == 0)
+            {
+                count--;
+                if (i + count < arr.size())
+            {
+                arr[i + count] = arr[i];
+            }
+            }
+        }
+    }
+};
