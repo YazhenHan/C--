fa481642f705122f7626935be34a52af4230320a
YazhenHan <yazhenhan@foxmail.com>
2021-01-19 14:50:45 +0800

    Leetcode 985

--- Leetcode2/SumEvenAfterQueries/SumEvenAfterQueries.cpp
+++ Leetcode2/SumEvenAfterQueries/SumEvenAfterQueries.cpp
@@ -8,26 +8,16 @@ public:
         int sum = 0;
         for (auto& a : A)
             if (a % 2 == 0)
-            {
                 sum += a;
-            }
         vector<int> ans;
         for (auto& query : queries)
         {
             int val = query[0], index = query[1];
             if (A[index] % 2 == 0)
-            {
                 sum -= A[index];
-                A[index] += val;
-            }
-            else
-            {
-                A[index] += val;
-            }
+            A[index] += val;
             if (A[index] % 2 == 0)
-            {
                 sum += A[index];
-            }
             ans.push_back(sum);
         }
         return ans;
