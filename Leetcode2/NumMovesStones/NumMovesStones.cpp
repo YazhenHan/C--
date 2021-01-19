2060bde1b0cf8022d4f54de65829fa1d37778572
YazhenHan <yazhenhan@foxmail.com>
2021-01-19 15:27:36 +0800

    Leetcode 1033

---
+++ Leetcode2/NumMovesStones/NumMovesStones.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> numMovesStones(int a, int b, int c) {
+        int right = max(max(a, b), c);
+        int left = min(min(a, b), c);
+        int middle = a + b + c - right - left;
+        int ma = right - left - 2;
+        if (middle - left == 2 || right - middle == 2)
+        {
+            mi = 1;
+        }
+        
+        int mi = min(ma, min(middle - left, min(right - middle, 2)));
+        vector<int> ans = {mi, ma};
+        return ans;
+    }
+};
