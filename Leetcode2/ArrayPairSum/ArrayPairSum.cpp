5936f7f4597489dec8935a3078ab6e3c62803ba2
YazhenHan <yazhenhan@foxmail.com>
2021-02-16 22:46:07 +0800

    Leetcode 561

---
+++ Leetcode2/ArrayPairSum/ArrayPairSum.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        for (size_t i = 0; i < nums.size(); i += 2)
+        {
+            ans += nums[i];
+        }
+        return ans;
+    }
+};
