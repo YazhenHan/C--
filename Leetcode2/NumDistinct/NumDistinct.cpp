27ed73d2812c5d0cc6f8138dccaa07516845311f
YazhenHan <yazhenhan@foxmail.com>
2021-03-17 20:05:59 +0800

    Leetcode 115

--- Leetcode2/NumDistinct/NumDistinct.cpp
+++ Leetcode2/NumDistinct/NumDistinct.cpp
@@ -1,26 +1,29 @@
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Solution {
-private:
-    int ans = 0;
-    void numD(string s, string t, int m, int n) {
-        if (n == t.size())
-        {
-            ans++;
-        }
-        for (int i = m; i < s.size(); i++)
-        {
-            if (s[i] == t[n])
-            {
-                numD(s, t, i + 1, n + 1);
-            }
-        }
-    }
 public:
     int numDistinct(string s, string t) {
-        numD(s, t, 0, 0);
-        return ans;
+        int m = s.size(), n = t.size();
+        if (m < n)
+            return 0;
+        vector<vector<long>> dp(m + 1, vector<long>(n + 1));
+        for (int i = 0; i <= m; i++)
+            dp[i][n] = 1;
+        for (int i = m - 1; i >= 0; i--)
+        {
+            char sChar = s.at(i);
+            for (int j = n - 1; j >= 0; j--)
+            {
+                char tChar = t.at(j);
+                if (sChar == tChar)
+                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+                else
+                    dp[i][j] = dp[i + 1][j];
+            }
+        }
+        return dp[0][0];
     }
 };
