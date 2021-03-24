6fd9d7b79b2e9f727a0289fc9fe978e7ba1e8713
YazhenHan <yazhenhan@foxmail.com>
2021-03-24 14:11:47 +0800

    Leetcode 456

--- Leetcode2/Find132pattern/Find132pattern.cpp
+++ Leetcode2/Find132pattern/Find132pattern.cpp
@@ -1,10 +1,29 @@
 #include <vector>
+#include <stack>
 
 using namespace std;
 
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
-        
+        int n = nums.size();
+        stack<int> candidate_k;
+        candidate_k.push(nums[n-1]);
+        int max_k = INT32_MIN;
+
+        for (int i  = n - 2; i >= 0; --i)
+        {
+            if (nums[i] < max_k)
+                return true;
+            while (!candidate_k.empty() && nums[i] > candidate_k.top())
+            {
+                max_k = candidate_k.top();
+                candidate_k.pop();
+            }
+            if (nums[i] > max_k)
+                candidate_k.push(nums[i]);
+        }
+
+        return false;
     }
 };
