3f1af79713c3199a77355db70853fa412c494dc8
YazhenHan <yazhenhan@foxmail.com>
2021-09-29 11:53:09 +0800

    Leetcode 787

--- Leetcode/Leetcode3/CorpFlightBookings/CorpFlightBookings.cpp
+++ Leetcode/Leetcode3/CorpFlightBookings/CorpFlightBookings.cpp
@@ -6,9 +6,13 @@ class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
         vector<int> v(n, 0);
-        for (auto& booking : bookings)
-            for (int i = booking[0]; i <= n && i <= booking[1]; i++)
-                v[i] += booking[2];
+        for (auto& booking : bookings) {
+            v[booking[0] - 1] += booking[2];
+            if (booking[1] < n)
+                v[booking[1]] -= booking[2];
+        }
+        for (int i = 1; i < n; i++)
+            v[i] = v[i - 1] + v[i];
         return v;
     }
 };
