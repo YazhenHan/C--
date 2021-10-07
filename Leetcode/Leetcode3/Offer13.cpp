61cc8f57ccb9d9a5830ebfeb4319b9e222078386
YazhenHan <yazhenhan@foxmail.com>
2021-10-07 10:10:35 +0800

    Offer13

---
+++ Leetcode/Leetcode3/Offer13.cpp
@@ -0,0 +1,39 @@
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+class Solution {
+private:
+    int getS(int m) {
+        int ans = 0;
+        while (m != 0) {
+            ans += m % 10;
+            m = m / 10;
+        }
+        return ans;
+    }
+public:
+    int movingCount(int m, int n, int k) {
+        int ans = 1;
+        queue<pair<int, int>> q;
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+        q.push({0, 0});
+        vis[0][0] = true;
+        while (!q.empty()) {
+            auto a = q.front();
+            if (a.first + 1 < m && !vis[a.first + 1][a.second] && getS(a.first + 1) + getS(a.second) <= k) {
+                q.push({a.first + 1, a.second});
+                vis[a.first + 1][a.second] = true;
+                ans++;
+            }
+            if (a.second + 1 < n && !vis[a.first][a.second + 1] && getS(a.first) + getS(a.second + 1) <= k) {
+                q.push({a.first, a.second + 1});
+                vis[a.first][a.second + 1] = true;
+                ans++;
+            }
+            q.pop();
+        }
+        return ans;
+    }
+};
