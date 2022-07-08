b54359d03f943e5ffd99fb57c04e31411f34f9ab
YazhenHan <yazhenhan@foxmail.com>
2022-07-08 20:59:58 +0800

    Create Oppo1.cpp

---
+++ Leetcode/Leetcode4/Oppo1.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        if (flowerbed.size() == 1)
+            return n == 0 || (flowerbed[0] == 0 && n == 1);
+        if (flowerbed[1] == 0) {
+            if (flowerbed[0] == 0) {
+                flowerbed[0] = 1;
+                n--;
+            }
+        }
+        for (int i = 1; i < flowerbed.size() - 1; i++) {
+            if (flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0) {
+                if (flowerbed[i] == 0) {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }
+        }
+        if (flowerbed[flowerbed.size() - 2] == 0) {
+            if (flowerbed[flowerbed.size() - 1] == 0) {
+                flowerbed[flowerbed.size() - 1] = 1;
+                n--;
+            }
+        }
+        return n <= 0;
+    }
+};
