b6ee7dcc6ecd4a68fc88d7a7ee6f1b46bad8147d
YazhenHan <yazhenhan@foxmail.com>
2022-07-24 15:56:56 +0800

    Create 354.cpp

---
+++ Leetcode/Leetcode4/354.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(), envelopes.end());
+        vector<int> dp(envelopes.size(), 0);
+        int ans = 0;
+        for (int i = 0; i < envelopes.size(); i++) {
+            int temp = 0;
+            for (int j = 0; j < i; j++)
+                if (envelopes[j][0] < envelopes[i][0] && envelopes[j][1] < envelopes[i][1])
+                    temp = max(temp, dp[j]);
+                else if (envelopes[j][0] >= envelopes[i][0])
+                    break;
+            dp[i] = temp + 1;
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+};
