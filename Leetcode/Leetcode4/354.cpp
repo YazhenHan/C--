9b0d2bf4d1121960a16139f1a5d8377384b18de6
YazhenHan <yazhenhan@foxmail.com>
2022-08-17 09:18:31 +0800

    Update 354.cpp

--- Leetcode/Leetcode4/354.cpp
+++ Leetcode/Leetcode4/354.cpp
@@ -21,4 +21,24 @@ public:
         }
         return ans;
     }
+
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        if (envelopes.empty()) return 0;
+        int n = envelopes.size();
+        sort(envelopes.begin(), envelopes.end(), [](const auto & e1, const auto & e2) {
+            return e1[0] < e2[0] || (e1[0] == e2[0] && e1[1] > e2[1]);
+        });
+
+        vector<int> f = {envelopes[0][1]};
+        for (int i = 1; i < envelopes.size(); i++) {
+            int num = envelopes[i][1];
+            if (num > f.back())
+                f.push_back(num);
+            else {
+                auto it = lower_bound(f.begin(), f.end(), num);
+                *it = num;
+            }
+        }
+        return f.size();
+    }
 };
