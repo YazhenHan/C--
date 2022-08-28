300ca03adcf3695659c67e1eaf1146bbb88fd781
YazhenHan <yazhenhan@foxmail.com>
2022-08-28 10:37:58 +0800

    Update 793.cpp

--- Leetcode/Leetcode5/793.cpp
+++ Leetcode/Leetcode5/793.cpp
@@ -8,11 +8,21 @@ private:
         }
         return ans;
     }
+
+    long long help(int k) {
+        long long r = 5LL * k;
+        long long l = 0;
+        while (l <= r) {
+            long long mid = (l + r) / 2;
+            if (trailingZeroes(mid) < k)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return r + 1;
+    }
 public:
     int preimageSizeFZF(int k) {
-        int t = 1;
-        while (trailingZeroes(t) < k)
-            t *= 5;
-        return t;
+        return help(k + 1) - help(k);
     }
 };
