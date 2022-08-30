8781cd276bc51d74018acb25cb8fd8de4bbf6451
YazhenHan <yazhenhan@foxmail.com>
2022-08-30 08:55:09 +0800

    Update 413.cpp

--- Leetcode/Leetcode5/413.cpp
+++ Leetcode/Leetcode5/413.cpp
@@ -8,7 +8,6 @@ public:
         if (nums.size() < 3) return 0;
         int ans = 0;
         int sub = nums[1] - nums[0], left = 0;
-        vector<int> dp(nums.size());
         for (int i = 2; i < nums.size(); i++) {
             int temp = nums[i] - nums[i - 1];
             if (temp == sub) {
