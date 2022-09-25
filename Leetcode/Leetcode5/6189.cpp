6b4a55f99c6142cabcb0b60cb5277c70cb157fe8
YazhenHan <yazhenhan@foxmail.com>
2022-09-25 11:08:15 +0800

    Update 6189.cpp

--- Leetcode/Leetcode5/6189.cpp
+++ Leetcode/Leetcode5/6189.cpp
@@ -5,20 +5,25 @@ using namespace std;
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int ans = 0, 
+        int ans = 1, temp = nums[0];
         vector<int> dp(nums.size()), dp2(nums.size());
         dp[0] = nums[0];
         dp2[0] = 1;
         for (int i = 1; i < nums.size(); i++) {
             int t = nums[i] & dp[i - 1];
-            if (t >= dp[i] - 1) {
+            if (t >= nums[i]) {
                 dp[i] = t;
-                dp2[i] = dp[i - 1] + 1;
+                dp2[i] = dp2[i - 1] + 1;
             } else {
                 dp[i] = nums[i];
                 dp2[i] = 1;
             }
-            ans = max(ans, dp2[i]);
+            if (dp[i] > temp) {
+                temp = dp[i];
+                ans = dp2[i];
+            } else if (dp[i] == temp) {
+                ans = max(ans, dp2[i]);
+            }
         }
         return ans;
     }
