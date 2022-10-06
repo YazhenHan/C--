e56a197fd2047e147eeac48c05b36f2fdf51efd6
YazhenHan <yazhenhan@foxmail.com>
2022-10-06 10:50:19 +0800

    Update 927.cpp

--- Leetcode/Leetcode6/927.cpp
+++ Leetcode/Leetcode6/927.cpp
@@ -28,3 +28,38 @@ public:
         return {i, j};
     }
 };
+
+class Solution {
+public:
+    vector<int> threeEqualParts(vector<int>& arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] == 1)
+                sum++;
+        }
+        if (sum % 3 != 0)
+            return {-1, -1};
+        if (sum == 0)
+            return {0, 2};
+        int first, second, third, cur = 0, partial = sum / 3;
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] == 1) {
+                if (cur == 0)
+                    first = i;
+                else if (cur == partial)
+                    second = i;
+                else if (cur == partial * 2)
+                    third = i;
+                cur++;
+            }
+        }
+        int len = arr.size() - third;
+        if (first + len > second || second + len > third)
+            return {-1, -1};
+        for (int i = 0; i < len && first + i < second && second + i < third; i++) {
+            if (arr[first + i] != arr[second + i] || arr[second + i] != arr[third + i])
+                return {-1, -1};
+        }
+        return {first + len - 1, second + len};
+    }
+};
