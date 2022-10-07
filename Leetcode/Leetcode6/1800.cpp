d3d1d50c0729259f923b4e0822379205197be08a
YazhenHan <yazhenhan@foxmail.com>
2022-10-07 09:51:57 +0800

    Create 1800.cpp

---
+++ Leetcode/Leetcode6/1800.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int ans = nums[0], temp = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > nums[i - 1])
+                temp += nums[i];
+            else
+                temp = nums[i];
+            ans = max(ans, temp);
+        }
+        return ans;
+    }
+};
