39566fce49e9a272a1e7d76a4e1442e859c10af4
YazhenHan <yazhenhan@foxmail.com>
2022-11-08 09:49:59 +0800

    Create 301.cpp

---
+++ Leetcode/Leetcode6/301.cpp
@@ -0,0 +1,53 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+private:
+    vector<string> ans;
+    inline bool isValid(const string & str) {
+        int cnt = 0;
+        for (int i = 0; i < str.size(); i++) {
+            if (str[i] == '(')
+                cnt++;
+            else if (str[i] == ')') {
+                cnt--;
+                if (cnt < 0)
+                    return false;
+            }
+        }
+        return cnt == 0;
+    }
+    void helper(string str, int start, int lremove, int rremove) {
+        if (lremove == 0 && rremove == 0) {
+            if (isValid(str))
+                ans.push_back(str);
+            return;
+        }
+        for (int i = start; i < str.size(); i++) {
+            if (i != start && str[i] == str[i - 1]) continue;
+            if (lremove + rremove > str.size() - i) return;
+            if (lremove > 0 && str[i] == '(')
+                helper(str.substr(0, i) + str.substr(i + 1), i, lremove - 1, rremove);
+            if (rremove > 0 && str[i] == ')')
+                helper(str.substr(0, i) + str.substr(i + 1), i, lremove, rremove - 1);
+        }
+    }
+public:
+    vector<string> removeInvalidParentheses(string s) {
+        int lremove = 0;
+        int rremove = 0;
+        for (char c : s) {
+            if (c == '(')
+                lremove++;
+            else if (c == ')')
+                if (lremove == 0)
+                    rremove++;
+                else
+                    lremove--;
+        }
+        helper(s, 0, lremove, rremove);
+        return ans;
+    }
+};
