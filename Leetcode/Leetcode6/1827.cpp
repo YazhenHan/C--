54d4f888d6cba16b0d8ca5ae7bd566c9b338cf83
YazhenHan <yazhenhan@foxmail.com>
2022-12-13 14:49:39 +0800

    Create 1827.cpp

---
+++ Leetcode/Leetcode6/1827.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int ans = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] <= nums[i - 1]) {
+                ans += (nums[i - 1] + 1 - nums[i]);
+                nums[i] = nums[i - 1] + 1;                
+            }
+        }
+        return ans;
+    }
+};
