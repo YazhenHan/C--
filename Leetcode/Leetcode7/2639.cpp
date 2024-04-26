2e4a0fbcecd4937bec32d191ce80951d9c328f4d
YazhenHan <yazhenhan@foxmail.com>
2024-04-27 04:21:26 +0800

    2639.cpp

---
+++ Leetcode/Leetcode7/2639.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findColumnWidth(vector<vector<int>>& grid) {
+        vector<int> ans = vector<int>(grid[0].size(), 0);
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                ans[j] = max(ans[j], int(to_string(grid[i][j]).length()));
+            }
+        }
+        return ans;
+    }
+};
