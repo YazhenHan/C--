b12e5c15a4e40195bd99f18d828536488db18473
YazhenHan <yazhenhan@foxmail.com>
2024-04-30 00:07:31 +0800

    2798.cpp

---
+++ Leetcode/Leetcode7/2798.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int ans = 0;
+        for (auto hour : hours) {
+            if (hour >= target)
+                ans++;
+        }
+        return ans;
+    }
+};
