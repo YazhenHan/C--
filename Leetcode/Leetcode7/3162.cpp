d572e2ef86035c9148db9198d522760a7c921ebe
YazhenHan <yazhenhan@foxmail.com>
2024-10-11 11:36:19 +0800

    3162.cpp

--- Leetcode/Leetcode7/3162.cpp
+++ Leetcode/Leetcode7/3162.cpp
@@ -1,15 +1,24 @@
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
-    int numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        int ans = 0;
+    long long numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        long long ans = 0;
+        unordered_map<int, int> um;
         for (int i = 0; i < nums1.size(); i++) {
-            for (int j = 0; j < nums2.size(); j++) {
-                if (nums1[i] % (nums2[j] * k) == 0) ans++;
+            if (um.count(nums1[i])) {
+                ans += um[nums1[i]];
+                continue;
             }
+            int temp = 0;
+            for (int j = 0; j < nums2.size(); j++) {
+                if (nums1[i] % (nums2[j] * k) == 0) 
+                    temp++;
+            }
+            um[nums1[i]] = temp;
         }
         return ans;
     }
