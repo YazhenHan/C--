9c7059ed7394928024c1e4484610f50e18a31958
YazhenHan <yazhenhan@foxmail.com>
2024-11-04 19:13:26 +0800

    56.cpp

---
+++ Leetcode/Leetcode8/56.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
+        ans.emplace_back(intervals[0]);
+        for (int i = 1; i < intervals.size(); i++) {
+            vector<int> temp = ans.back();
+            if (temp[1] >= intervals[i][0]) {
+                ans[ans.size() - 1][1] = max(ans[ans.size() - 1][1], intervals[i][1]);
+            } else {
+                ans.emplace_back(intervals[i]);
+            }
+        }
+        return ans;
+    }
+};
