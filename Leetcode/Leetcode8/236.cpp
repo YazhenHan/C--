ccdaa80cc6f17e9b6f124f2334825bcf1b259d11
YazhenHan <yazhenhan@foxmail.com>
2024-11-10 21:20:40 +0800

    236.cpp

---
+++ Leetcode/Leetcode8/236.cpp
@@ -0,0 +1,24 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+private:
+    TreeNode* ans;
+    bool dfs(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr) return false;
+        bool lson = dfs(root->left, p, q);
+        bool rson = dfs(root->right, p, q);
+        if ((lson && rson) || ((root == p || root == q) && (lson || rson)))
+            ans = root;
+        return lson || rson || (root == p || root == q);
+    }
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        dfs(root, p, q);
+        return ans;
+    }
+};
