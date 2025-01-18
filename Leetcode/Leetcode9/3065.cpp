e37cf63503557a452c25227216afa8ad91e473e9
YazhenHan <yazhenhan@foxmail.com>
2025-01-18 14:15:22 +0800

    3065.cpp

---
+++ Leetcode/Leetcode9/3065.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int ans = 0;
+        for (int num : nums) {
+            if (num < k) ans++;
+        }
+        return ans;
+    }
+};
