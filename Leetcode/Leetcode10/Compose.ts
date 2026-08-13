75bf57f2534280ced787c74a4eb01e550771a052
YazhenHan <yazhenhan@foxmail.com>
2026-08-14 01:47:40 +0800

    2629 compose

---
+++ Leetcode/Leetcode10/Compose.ts
@@ -0,0 +1,16 @@
+type F = (x: number) => number;
+
+function compose(functions: F[]): F {
+    
+	return function(x) {
+        for (let i = 0; i < functions.length; i++) {
+            x = functions[functions.length - 1 - i](x);
+        }
+        return x;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
