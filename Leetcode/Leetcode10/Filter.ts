8746ae094994b8cc6247438ad4a7864c375cfa6c
YazhenHan <yazhenhan@foxmail.com>
2026-08-14 01:39:35 +0800

    2634 Filter

---
+++ Leetcode/Leetcode10/Filter.ts
@@ -0,0 +1,9 @@
+type Fn = (n: number, i: number) => any
+
+function filter(arr: number[], fn: Fn): number[] {
+    const ans = new Array();
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) ans.push(arr[i]);
+    }
+    return ans;
+};
